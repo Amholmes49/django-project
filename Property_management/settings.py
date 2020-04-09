bf109e92964a15f6b2b13de189db8f8ad5673eaa
Aaron Holmes <HolmesAaron@Johndeere.com>
2020-04-09 13:34:05 -0500

    user/Password

--- Property_management/settings.py
+++ Property_management/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '_n$%2rw-*n+k%t=yzbhzq9yhq!ussc_6(s%3u#e$6s5*5-fis+'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '_n$%2rw-*n+k%t=yzbhzq9yhq!ussc_6(s%3u#e$6s5*5-fis+'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rental-hub.herokuapp.com']
+ALLOWED_HOSTS = ['rental-hub.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rentalhub',
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'USER': 'rentalhubuser',
+        'PASSWORD': 'rentalhub',
         'HOST': 'localhost'
     }
 }
